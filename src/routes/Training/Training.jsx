3f67cfcf843e39293ebd629e5ba1be9a61aef7dc
Charity <umercy001@gmail.com>
2023-11-16 14:32:31 +0100

    made some changes to the training page

--- src/routes/Training/Training.jsx
+++ src/routes/Training/Training.jsx
@@ -12,7 +12,7 @@ export const Training = () =>
 
     <p className='venue'> Venue: Center for Energy Research and Development, OAU</p>
 
-    <p className='date'> Date: 27th - 30th November, 2023</p>
+    <p className='date'> Date: 27th - 30th November, 2023 || 4th - 7th December 2023</p>
 
     <p className='regLink'>Use this <a href='https://ztenig.com/training'>link</a> to register </p>
   
