d90629b110856a6606fa9301ec131839e8d0be95
Charity <umercy001@gmail.com>
2023-11-16 14:29:36 +0100

    Modify the training page

--- src/routes/Training/Training.jsx
+++ src/routes/Training/Training.jsx
@@ -1,3 +1,22 @@
 import './training.css'
+import training from '../../assets/images/training.jpg'
 
-export const Training = () => <h2> The Training page</h2>
+
+export const Training = () => 
+{
+  return(
+    <div className='training'>
+
+    <h1 className='training-title'>Upcoming Trainings</h1>
+    <img className='training-img' src={training} alt='Vehicle and generator training'></img>
+
+    <p className='venue'> Venue: Center for Energy Research and Development, OAU</p>
+
+    <p className='date'> Date: 27th - 30th November, 2023</p>
+
+    <p className='regLink'>Use this <a href='https://ztenig.com/training'>link</a> to register </p>
+  
+      </div>
+  )
+}
+
